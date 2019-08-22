a5486dd83361b5eda549145f5fb2f9c7eb8bc7db
cccastro09 <castro_2003_007@hotmail.com>
2019-08-22 12:44:22 -0500

    nuevo form y diseño

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { EspolListComponent } from './components/espol-list/espol-list.component';
+import { FormComponent } from './components/form/form.component';
 import { EspolService } from './components/servicios/espol.service'
 import { HttpClientModule}  from '@angular/common/http';
 
@@ -12,6 +13,7 @@ import { HttpClientModule}  from '@angular/common/http';
   declarations: [
     AppComponent,
     NavigationComponent,
+    FormComponent,
     EspolListComponent
   ],
   imports: [
